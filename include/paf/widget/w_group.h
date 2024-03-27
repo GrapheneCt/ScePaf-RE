936c255126096e04597bc03759e2f2df68c00d3f
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2024-03-27 13:58:46 +0900

    Added Group class

--- include/paf/widget/w_group.h
+++ include/paf/widget/w_group.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
 	Vita Development Suite Libraries
 */
 
@@ -8,5 +8,28 @@
 #include <paf/widget/factory.h>
 
 __declspec(dllimport) extern paf::ui::FactoryFuncTable g_factoryGroup;
+
+namespace paf {
+
+	namespace ui {
+
+		class Group : public Widget
+		{
+		public:
+
+			static const char *TypeName(void);
+
+			virtual ~Group();
+
+			virtual bool IsInherit(char const *) const;
+			virtual char *GetTypeName(void) const;
+
+		protected:
+			unsigned char m_unk_0x27C[4];
+
+			__declspec (dllimport) static const char m_widget_type[];
+		};
+	}
+}
 
 #endif /* _VDSUITE_USER_PAF_WIDGET_W_GROUP_H */
