ce5a2c383d75c47c54228e31d91656d59a17e3c1
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2024-03-27 13:50:04 +0900

    Added PageTurn class

--- include/paf/widget/w_pageturn.h
+++ include/paf/widget/w_pageturn.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
 	Vita Development Suite Libraries
 */
 
@@ -6,7 +6,33 @@
 #define _VDSUITE_USER_PAF_WIDGET_W_PAGETURN_H
 
 #include <paf/widget/factory.h>
+#include <paf/widget/w_pageturn_base.h>
 
 __declspec(dllimport) extern paf::ui::FactoryFuncTable g_factoryPageTurn;
+
+namespace paf {
+
+	namespace ui {
+
+		class PageTurn : public PageTurnBase
+		{
+		public:
+
+			static const char *TypeName(void);
+
+			PageTurn(Widget *_parent, CreateParam *_param);
+
+			virtual ~PageTurn();
+
+			virtual bool IsInherit(char const *) const;
+			virtual char *GetTypeName(void) const;
+
+		protected:
+			unsigned char m_unk_0x2B0[0x48];
+
+			__declspec (dllimport) static const char m_widget_type[];
+		};
+	}
+}
 
 #endif /* _VDSUITE_USER_PAF_WIDGET_W_PAGETURN_H */
